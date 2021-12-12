5cd1e150236e0bcdbb22b8d0026a25008631b1e7
Adrien Marques <marquesadrien.pro@gmail.com>
2021-12-12 23:47:03 +0000

    Main:Ajout Visuels

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { FormsModule,  ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
 
 import { HeaderComponent } from './general/header/header.component';
 import { UsersListComponent } from './components/users-list/users-list.component';
 import { UsersCreateComponent } from './components/users-create/users-create.component';
 import { UsersDeleteComponent } from './components/users-delete/users-delete.component';
 import { UsersUpdateComponent } from './components/users-update/users-update.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -27,7 +33,13 @@ import { UsersUpdateComponent } from './components/users-update/users-update.com
     HttpClientModule,
     NgbModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatSelectModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
